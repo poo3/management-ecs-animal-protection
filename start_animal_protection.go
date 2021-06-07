691934fd678790d8f55c60f6c2ebcac8346ad0be
バントウフウマ <coolishpoo3@gmail.com>
2021-06-07 17:13:16 +0900

    ecsサービスを自動で起動・停止する為にlambdaにアップロードしたソース

---
+++ start_animal_protection.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecs"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+)
+
+const desiredCount int64  = 1
+
+var cluster = "animal-protection-cluster-frontend"
+
+func main() {
+	lambda.Start(startServies)
+}
+
+func startServies() {
+	services := []string{
+		"animal-protection-service-backend",
+		"animal-protection-service-frontend",
+	}
+
+	for _, service := range services {
+		svc := ecs.New(session.New())
+		input := &ecs.UpdateServiceInput{
+			Cluster:        aws.String(cluster),
+			Service:        aws.String(service),
+			DesiredCount:   aws.Int64(desiredCount),
+		}
+		result, err := svc.UpdateService(input)
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				fmt.Println(aerr.Code(), aerr.Error())
+			}
+			return
+		}
+		fmt.Println(result)
+	}
+}
